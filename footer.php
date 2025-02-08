cd108d8f7786c196ea345d1dc80a01c76e69c277
Sreeja Satish <sreejasatish.drexel@gmail.com>
2025-02-08 01:59:38 -0500

    update

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
+
 <footer>
     <h1>Ready to Shoot for the Stars?</h1>
     <h1>Let's Connect!</h1>
 
+    <div class="footer-icons">
         <a href="https://www.linkedin.com/in/sreejasatish/" class="icons" target="_blank">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/linkedin-icon.svg" alt="linkedin">
         </a>
@@ -11,6 +13,8 @@
         <a href="https://github.com/sreejas36" class="icons" target="_blank">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/github-icon.svg" alt="github">
         </a>
+    </div>
+
 
     <p>&copy; 2025 Sreeja Satish</p>
 </footer>
