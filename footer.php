c5a753f7651d38432d9792715c0620fdf2d3e1fb
Sreeja Satish <sreejasatish.drexel@gmail.com>
2025-02-20 12:50:29 -0500

    update

--- footer.php
+++ footer.php
@@ -1,22 +1,29 @@
 
 <footer>
-    <h1>Ready to Shoot for the Stars?</h1>
-    <h1>Let's Connect!</h1>
-
-    <div class="footer-icons">
-        <a href="https://www.linkedin.com/in/sreejasatish/" class="icons" target="_blank">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/linkedin-icon.svg" alt="linkedin">
-        </a>
-        <a href="mailto:sreeja.satish@drexel.edu" class="icons" target="_blank">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/mail-icon.svg" alt="mail">
-        </a>
-        <a href="https://github.com/sreejas36" class="icons" target="_blank">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/github-icon.svg" alt="github">
-        </a>
+    <div class="footer-left">
+        <img src="/assets/large-star.png" alt="">
     </div>
+    <div class="footer-content">
+        <h1>Ready to Shoot for the Stars?</h1>
+        <h1>Let's Connect!</h1>
+
+        <div class="footer-icons">
+            <a href="https://www.linkedin.com/in/sreejasatish/" class="icons" target="_blank">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/linkedin-icon.svg" alt="linkedin">
+            </a>
+            <a href="mailto:sreeja.satish@drexel.edu" class="icons" target="_blank">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/mail-icon.svg" alt="mail">
+            </a>
+            <a href="https://github.com/sreejas36" class="icons" target="_blank">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/github-icon.svg" alt="github">
+            </a>
+        </div>
+
+        <p>&copy; 2025 Sreeja Satish</p>
+    </div>
+    
 
 
-    <p>&copy; 2025 Sreeja Satish</p>
 </footer>
 <?php wp_footer (); ?>
 </body>
